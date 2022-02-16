fdfed283883011fd82fbc5d090b0c69b1789af33
Alejandra <Ale@MacBook-Pro-de-Ale.local>
2022-02-16 13:17:46 -0700

    Shell progress

--- shell/myShell.py
+++ shell/myShell.py
@@ -2,30 +2,75 @@ import os
 import sys
 import re
 
+def chdir(dir):
+
+
+    try:
+        os.chdir(dir)
+
+    except FileNotFoundError:
+
+        os.write(0, ("Path not found\n").encode())
+
+def ls():
+    list = os.listdir()
+    print()
+
+    for dir in list:
+        print(dir + " ")
+
+
 # Create a child process
 # using os.fork() method
-print("starting pid")
-pid = os.fork()
-print("pid done")
+def ex():
+    print("starting pid")
+    pid = os.getpid()
+    print("pid done")
 
-# pid greater than 0 represents
-# the parent process
-if pid > 0:
-    print("I am parent process:")
-    print("Process ID:", os.getpid())
-    print("Child's process ID:", pid)
-    print("in parent")
+    print("Forking...")
+    fk = os.fork()
 
-# pid equal to 0 represents
-# the created child process
-else:
-    print("\nI am child process:")
-    print("Process ID:", os.getpid())
-    print("Parent's process ID:", os.getppid())
-    print("In child")
+    # pid greater than 0 represents
+    # the parent process
+    if fk < 0:
+        os.write(0, ("fork failed, returning...\n").encode())
+        sys.exit(1)
+    
+    print("Forked succesfully.")
 
-print("out")
+    if fk > 0:
+        print("I am parent process:")
+        print("Process ID:", os.getpid())
+        print("Child's process ID:", pid)
+        print("in parent")
 
-# If any error occurred while
-# using os.fork() method
-# OSError will be raised
+    # pid equal to 0 represents
+    # the created child process
+    else:
+        print("\nI am child process:")
+        print("Process ID:", os.getpid())
+        print("Parent's process ID:", os.getppid())
+        print("In child")
+
+    print("out")
+
+    # If any error occurred while
+    # using os.fork() method
+    # OSError will be raised
+
+while(True):
+    rawinp = str(input("$"))
+
+    inp = rawinp.split()
+
+    if (inp[0] == "exit"):
+        sys.exit(0)
+
+    if (inp[0] == "ls"):
+        ls()
+
+    if (inp[0] == "cd"):
+        dir = inp[1]
+        chdir(dir)
+
+
